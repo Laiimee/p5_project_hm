3183a1a17c8100c2b0754eb4732fe3130949afd8
Laiimee <honorine.magallon@gmail.com>
2022-08-05 00:55:29 +0200

    calcule Panier dans cart.js

--- P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -6,7 +6,7 @@ function getProductById(id){
 
 function refreshCart(cart){
     localStorage.setItem('cart', JSON.stringify(cart));
-}
+  }
 
 function getCart() {
     const cart = localStorage.getItem('cart');
@@ -17,8 +17,8 @@ function getCart() {
 }
 
 function removeCartItem(article){
-    let cart = getCart();
-    cart = cart.filter(item => item.id !== article.dataset.id || item.color !==  article.dataset.color || item.quantity !== article.dataset.quantity); 
+    let cart = getCart(); 
+    cart = cart.filter(item => item.id !== article.dataset.id || item.color !==  article.dataset.color); 
     refreshCart(cart);
 }
 
@@ -26,104 +26,140 @@ function removeProductHTML(article){
     article.remove();
 }
 
-function addProductToHTML(product,item,total,totalqty){
-
-  console.log(total);
-  const article = document.createElement('article');
-  article.className ='cart__item';
-  article.dataset.id = item.id;
-  article.dataset.color = item.color;
-  
-  const cart__item__img = document.createElement('div');
-  cart__item__img.className = 'cart__item__img';
-  cart__item__img.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
-
-  const cart__item__content = document.createElement('div');
-  const cart__item__content__description = document.createElement('div');
-  cart__item__content.className = 'cart__item__content';
-  cart__item__content__description.className = 'cart__item__content__description';
-  cart__item__content__description.innerHTML=`
-    <h2>${product.name}</h2>
-    <p>${item.color}</p>
-    <p>${product.price}</p>`;
-  cart__item__content.append(cart__item__content__description);
-
-  const cart__item__content__settings = document.createElement('div');
-  const cart__item__content__settings__quantity = document.createElement('div');
-  cart__item__content__settings.className = 'cart__item__content__settings';
-  cart__item__content__settings__quantity.className = 'cart__item__content__settings__quantity';
-  cart__item__content__settings__quantity.innerHTML = `
-    <p>Qté : </p>
-    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">`;
-  cart__item__content.append(cart__item__content__settings);
-  cart__item__content__settings.append(cart__item__content__settings__quantity);
-
-  const cart__item__content__settings__delete = document.createElement('div');
-  cart__item__content__settings__delete.className = 'cart__item__content__settings__delete';
-  const deleteItem = document.createElement('p');
-  deleteItem.className = 'deleteItem';
-  deleteItem.innerText = 'Supprimer';
-  cart__item__content__settings.append(cart__item__content__settings__delete);
-
-  cart__item__content__settings__delete.append(deleteItem);
-    article.append(cart__item__img);
-    article.append(cart__item__content);
-  document.getElementById('cart__items').append(article);
-
-
-  const totalPrice = document.getElementById('totalPrice');
-  totalPrice.innerHTML = total;
-
-  const totalQuantity = document.getElementById('totalQuantity');
-  totalQuantity.innerHTML = totalqty;
-  
-  deleteItem.addEventListener('click', () => {
-        removeCartItem(article, total, totalqty);
-        removeProductHTML(article, total, totalqty);
-    })
+function getParent(element, num) {
+    let parent = element;
+    for (let i = 0; i < num; i++) {
+        if (parent.parentNode) {
+            parent = parent.parentNode;
+        }
+    }
+    return parent; 
 }
 
-//  Calculer le panier & quantité //
+function setTotal(array, elemPrice, elemQuantity)
+{
+    array.total.totalQteItems = getTotal(array, 'quantity');
+    array.total.totalItems = getTotal(array); 
 
-function getTotalPrice(items){
-    let totalPrice = 0; 
-    for (const item of items){
-        totalPrice += item.total;
-    }
-    return totalPrice;
+    elemPrice.innerHTML = array.total.totalItems; 
+    elemQuantity.innerHTML = array.total.totalQteItems; 
 }
 
-function getTotalQuantity(items){
-    let totalQuantity = 0; 
-    for (const bloup of items){
-        totalQuantity += bloup.item.quantity;
+function addProductToHTML(datas){
+
+  for(const elem of datas) 
+    {
+        const article = document.createElement('article');
+        article.className ='cart__item';
+        article.dataset.id = elem.item.id;
+        article.dataset.color = elem.item.color;
+        
+        const cart__item__img = document.createElement('div');
+        cart__item__img.className = 'cart__item__img';
+        cart__item__img.innerHTML = `<img src="${elem.product.imageUrl}" alt="${elem.product.altTxt}">`;
+    
+        const cart__item__content = document.createElement('div');
+        const cart__item__content__description = document.createElement('div');
+        cart__item__content.className = 'cart__item__content';
+        cart__item__content__description.className = 'cart__item__content__description';
+        cart__item__content__description.innerHTML=`
+        <h2>${elem.product.name}</h2>
+        <p>${elem.item.color}</p>
+        <p>${elem.product.price}€</p>`;
+        cart__item__content.append(cart__item__content__description);
+    
+        const cart__item__content__settings = document.createElement('div');
+        const cart__item__content__settings__quantity = document.createElement('div');
+        cart__item__content__settings.className = 'cart__item__content__settings';
+        cart__item__content__settings__quantity.className = 'cart__item__content__settings__quantity';
+        cart__item__content__settings__quantity.innerHTML = `
+        <p>Qté : </p>
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${elem.item.quantity}">`;
+        cart__item__content.append(cart__item__content__settings);
+        cart__item__content__settings.append(cart__item__content__settings__quantity);
+    
+        const cart__item__content__settings__delete = document.createElement('div');
+        cart__item__content__settings__delete.className = 'cart__item__content__settings__delete';
+        const deleteItem = document.createElement('p');
+        deleteItem.className = 'deleteItem';
+        deleteItem.innerText = 'Supprimer';
+        cart__item__content__settings.append(cart__item__content__settings__delete);
+    
+        cart__item__content__settings__delete.append(deleteItem);
+        article.append(cart__item__img);
+        article.append(cart__item__content);
+        document.getElementById('cart__items').append(article);
+        
+    
+        const totalPrice = document.getElementById('totalPrice'); 
+        totalPrice.innerHTML = datas.total.totalItems; 
+    
+        const totalQuantity = document.getElementById('totalQuantity'); 
+        totalQuantity.innerHTML = datas.total.totalQteItems; 
+    
+        deleteItem.addEventListener('click', () => {
+            removeCartItem(article);
+            removeProductHTML(article);
+            const index = datas.indexOf(elem);
+            datas.splice(index, 1); 
+            setTotal(datas, totalPrice, totalQuantity); 
+        })
+
+        
+        const inputQtes = document.querySelectorAll('.itemQuantity'); 
+        for(const inputQte of inputQtes)
+        {
+            inputQte.addEventListener('change', (e) => {
+                const elemId = getParent(inputQte, 4).dataset.id; 
+                const datasElem = datas.find(elem => elem.item.id == elemId); 
+                datasElem.item.quantity = parseInt(e.target.value); 
+                datasElem.total = datasElem.item.quantity * datasElem.product.price; 
+                setTotal(datas, totalPrice, totalQuantity);
+            }) 
+        }
+       
+        
     }
-    console.log(totalQuantity);
-    return totalQuantity;
+
+}
+
+
+function getTotal(datas, type = '')
+{
+    let total = 0; 
+    for (const item of datas){
+        if(type == 'quantity') 
+        {
+            total += item.item.quantity; 
+        }
+        else 
+        {
+            total += item.total; 
+        }
+    }
+
+    return total; 
 }
 
 async function initCartProducts(){
     const cart = getCart();
-    console.log(cart);
-    let total = 0;
+    let total = 0; 
     const datas = await Promise.all(cart.map(async item => {
         const product = await getProductById(item.id);
-        total = item.quantity * product.price;        
-        let obj = {item, product, total};
-        console.log(obj);
-        return {item, product, total}
+        total= item.quantity * product.price; 
+        let obj = {item, product, total}
+        return obj; 
     }));
 
-   
-    let totalItems = getTotalPrice(datas);
-    let totalqty =  getTotalQuantity(datas);
-    for (const data of datas){
-        addProductToHTML(data.product, data.item, totalItems, totalqty);
-    }
+    let totalItems = getTotal(datas); 
+    let totalQteItems = getTotal(datas, 'quantity'); 
+
+    datas['total'] = {totalItems, totalQteItems}; 
+
+    addProductToHTML(datas); 
 }
-
-
  function init() {
+
     initCartProducts();
     initForm();
 }
@@ -175,8 +211,11 @@ function buildFormData(formData, data, parentKey) {
   
 async function buyCart(cart,contact){
     try{
-        const response = await fetch(`http://localhost:3000/api/products/order`,{method:'post',headers:{"content-type":"application/x-www-form-urlencoded"}, body:jsonToFormDatas({contact,products:cart})});
-        console.log(response);
+        const response = await fetch(`http://localhost:3000/api/products/order`,
+        { method:'post',
+            headers:{"content-type":"application/json; charset=utf-8"}, 
+            body:jsonToFormData({contact,products:cart})});
+            console.log(response);
         if(response.statusCode == 200){
             redirectToConfirmation();
         }else{ alert("une erreur s'est produit");}
@@ -191,7 +230,7 @@ async function buyCart(cart,contact){
 function validForm({firstName,lastName,address,city,email}){
     let  isValide = true ;
 
-    if(!/^[\w\.\d]+@[\w\d]+\.[\w]+$/.test(email)){
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
         document.getElementById('emailErrorMsg').innerHTML="invalide";
         isValide = false;
     }
