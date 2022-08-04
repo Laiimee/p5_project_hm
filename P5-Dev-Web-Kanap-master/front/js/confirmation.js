5e465f87c3f08c5ff6ea61567bb64b15f514cd5b
Laiimee <honorine.magallon@gmail.com>
2022-08-04 21:56:11 +0200

    cart.js v3

--- P5-Dev-Web-Kanap-master/front/js/confirmation.js
+++ P5-Dev-Web-Kanap-master/front/js/confirmation.js
@@ -1,9 +1,18 @@
 // Afficher le numéro de commande dans la page confirmation //
 
-function getNumberOrders(id){
+/* function getNumberOrders(id){
     return fetch (`http://localhost:3000/api/products/order`).then(r => r.json());
 }
 
 function initNumberOrder(cart,contact){
     
 }
+ */
+// récupérer le localStorage
+const params = new URLSearchParams(location.search)
+let id = params.get("id")
+console.log(id)
+
+const orderId = document.getElementById("orderId")
+orderId.innerHTML = id
+localStorage.clear()
